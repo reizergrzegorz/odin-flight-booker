ccd0a1b182661a9cbec4a0395168d73c318d0af1
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-06 16:19:58 +0200

    add search params to flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,23 @@
 class FlightsController < ApplicationController
     def index
-        @airports = Airport.all
-        @flights = Flight.all
-    end    
-end
+      @airports = Airport.all
+      @flights = Flight.all
+  
+      if params[:departure_airport_id].present?
+        @flights = @flights.where(departure_airport_id: params[:departure_airport_id])
+      end
+  
+      if params[:arrival_airport_id].present?
+        @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id])
+      end
+  
+      if params[:date].present?
+        @flights = @flights.where("DATE(start_datetime) = ?", params[:date])
+      end
+  
+      if params[:passengers].present?
+        # Handle passengers filtering logic if needed
+      end
+    end
+  end
+  
