7996cf39c6edaa40f54c631188431f147625ad83
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-06 17:13:09 +0200

    refactor bookings resources

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,38 @@
 class BookingsController < ApplicationController
   def new
+    # Find the flight that was selected
     @flight = Flight.find(params[:flight_id])
     @num_passengers = params[:passengers].to_i
-    @booking = Booking.new
+
+    # Create a new booking with associated passengers
+    @booking = Booking.new(flight: @flight, num_passengers: @num_passengers)
+
+    # Create the required number of empty Passenger objects
+    @num_passengers.times { @booking.passengers.build }
   end
 
   def create
+    @flight = Flight.find(params[:booking][:flight_id])
+    @num_passengers = params[:booking][:num_passengers].to_i
+    @booking = Booking.new(booking_params)
+
+    # Save the booking and passengers
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @passengers = @booking.passengers
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :num_passengers, passengers_attributes: [:name, :email])
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'flights#index'
-  
+
   resources :flights, only: [:index]
-  resources :bookings, only: [:new, :create]
+  resources :bookings, only: [:new, :create, :show]
 end
 
